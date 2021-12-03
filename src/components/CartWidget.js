07903efd143fc9b2731d9814a1353bf589d989de
claudio <claudiomr79@gmail.com>
2021-12-03 16:43:16 -0300

    clase 4, corregido el navbar y responsive

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 export const CartWidget = () => {
-    return (
-      <div>
-          <li>
-          <a href="#"><i class="large material-icons"> search </i></a>
-          </li>
-          <li>
-          <a href="#"><i class="large material-icons"> shopping_cart </i></a>
-          </li>
-      </div>
-    );
+  return (
+    <div>
+      <li>
+        <a href="#">
+          <i className="large material-icons"> search </i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="large material-icons"> shopping_cart </i>
+        </a>
+      </li>
+    </div>
+  );
 }
--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -4,12 +4,11 @@ import { CartWidget } from '../CartWidget';
 export const NavBar = () => {
   return (
     <div className="navbar-fixed">
-      <nav className="black">
-        <div className="nav-wrapper container">
-          <a href="#" className="brand-logo left">
+      <nav className="nav-wrapper black">
+          <a href="#" style={{paddingLeft:30}} className="brand-logo">
             Cervemundo
           </a>
-          <ul className="right">
+          <ul style={{paddingRight:20}} className="right hide-on-med-and-down">
             <li>
               <a href="#">Productos</a>
             </li>
@@ -26,8 +25,16 @@ export const NavBar = () => {
                <CartWidget  />
             </li>
           </ul>
-        </div>
+          <a href="#" data-target="responsive-menu" className="sidenav-trigger right">
+            <i className="material-icons">menu</i>
+          </a>
       </nav>
+      <ul id="responsive-menu" className="sidenav">
+            <li><a href="#">Item1</a></li>
+            <li><a href="#">Item2</a></li>
+            <li><a href="#">Item3</a></li>
+            <li><a href="#">Item4</a></li>
+      </ul>
     </div>
   );
 };
